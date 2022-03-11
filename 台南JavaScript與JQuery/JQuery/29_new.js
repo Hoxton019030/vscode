9e80d3723bbcd48171793490f74a000e3790708c
Hoxton <za546321@yahoo.com.tw>
2022-03-11 16:27:17 +0800

    2022-3-11

--- 台南JavaScript與JQuery/JQuery/29_new.js
+++ 台南JavaScript與JQuery/JQuery/29_new.js
@@ -1,3 +1,9 @@
-$(function () {
-  
+$(function() {
+    let divWidth = $('#sliderBoard').width()
+    let imgCount = $('#content li').length
+
+    for (let i = 0; i < imgCount; i++) {
+
+    }
+
 });
