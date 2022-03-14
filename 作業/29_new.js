d8ab225523a3b116c8d2dbb4361956f10c11557d
Hoxton <za546321@yahoo.com.tw>
2022-03-15 01:44:07 +0800

    2022-3-15

---
+++ 作業/29_new.js
@@ -0,0 +1,40 @@
+ $(function(){
+     let divWidth = $('#sliderBoard').width()    // 抓寬度
+     let imgCount = $('#content li').length      // 查幾個圖
+
+     /* 生成 contentbutton 的 li */
+     for(let i = 0; i < imgCount; i++){
+        $('#contentButton').append(`<li></li>`)    
+     }
+
+     $('#contentButton li:nth-child(1)').addClass('clicked');
+
+     $('#content li').width(divWidth)           // li 的寬度
+     $('#content').width(divWidth * imgCount)   // ul 的寬度
+
+     let index;
+     $('#contentButton li').click(function(){
+         // alert($(this).index())
+         index = $(this).index()
+
+         $('#content').animate({
+             left: divWidth * index * -1,
+         })
+
+         $(this).addClass('clicked')
+         $('#contentButton li').not(this).removeClass('clicked')
+     })
+     
+     // 功能1: resize, 將所有 resize 之後會影響的值放在 changeStatus 函數中，當 window 發生 resize 事件時就去呼叫該函數
+     $(window).resize(function(){
+         changeStatus()
+     })
+     function changeStatus(){}
+
+     // 功能2: 每間隔8秒呼叫一次函數
+     setInterval(moveNext, 5000)
+     
+     function moveNext(){
+         // 偵測在第幾個 index, 移到下一個則++
+     }
+ });
--- 台南JavaScript與JQuery/作業繳交區/new.js
+++ 台南JavaScript與JQuery/作業繳交區/new.js
@@ -2,7 +2,7 @@ $(function () {
     let divWidth = $('#sliderBoard').width()
     let imgCount = $('#content li').length
 
-    for(let i = 0; i < imgCount; i++){
+    for (let i = 0; i < imgCount; i++) {
         $('#contentButton').append(`<li></li>`)
     }
     $('#contentButton li:nth-child(1)').addClass('clicked');
@@ -11,7 +11,7 @@ $(function () {
     $('#content').width(divWidth * imgCount)    // ul的寬度
 
     let index;
-    $('#contentButton li').click(function(){
+    $('#contentButton li').click(function () {
         index = $(this).index()
 
         $('#content').animate({
@@ -20,5 +20,19 @@ $(function () {
 
         $(this).addClass('clicked')
         $('#contentButton li').not(this).removeClass('clicked')
+
     })
-});
+    //每秒自動輪播
+    let num = 0;
+    setInterval(function () {
+        num++
+        let imgs = document.querySelectorAll('.img')
+        for (let i = 0; i < imgs.length; i++) {
+            imgs[i].style.display = 'none'
+        }
+        if (num > 7) {
+            num = 0;
+        }
+        imgs[num].style.display = 'block'
+    }, 1000);
+})
